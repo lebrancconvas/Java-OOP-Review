de11d6c4ebc04cba2b0bd1be4d5adc5e66476155
lebrancconvas <lebrancconvas@gmail.com>
2021-09-22 20:11:00 +0700

    :sparkles: Create Class Character.

--- java-oop/src/App.java
+++ java-oop/src/App.java
@@ -1,3 +1,33 @@
+class Character {
+    String Name;
+    String Food;
+    String Color;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setFood(String food) {
+        Food = food;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getFood() {
+        return Food;
+    }
+
+    public String getColor() {
+        return Color; 
+    }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
