3db3f5bec017145e91fdf8d8b6f4a1b11e14e2f1
lebrancconvas <lebrancconvas@gmail.com>
2021-09-23 01:10:54 +0700

    :sparkles: Add Food and Color.

--- java-oop/src/App.java
+++ java-oop/src/App.java
@@ -1,5 +1,5 @@
 class Character {
-    String Name;
+    String Name; 
     String Food;
     String Color;
 
@@ -24,7 +24,7 @@ class Character {
     }
 
     public String getColor() {
-        return Color; 
+        return Color;
     }
 }
 
@@ -32,6 +32,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Character char1 = new Character();
         char1.setName("Hajime");
-        System.out.println(char1.getName()); 
+        char1.setFood("Chocolate Moji");
+        char1.setColor("Majenta");
+        System.out.printf("I am %s, I love to eat %s and I like %s.", char1.getName(), char1.getFood(), char1.getColor()); 
     }
 }
