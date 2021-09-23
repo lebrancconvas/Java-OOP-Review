37797558f022eada6d86e6f7dcfbd95ada3eb05b
lebrancconvas <lebrancconvas@gmail.com>
2021-09-23 22:12:41 +0700

    :sparkles: Create Inheritance Class.

--- java-oop/src/App.java
+++ java-oop/src/App.java
@@ -31,12 +31,19 @@ class Character {
     }
 }
 
+class Player extends Character {
+ public void walk() {
+     System.out.println("Player is walking.");
+ }
+}
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Character char1 = new Character();
+        Player char1 = new Player(); 
         char1.setName("Hajime");
         char1.setFood("Chocolate Moji");
         char1.setColor("Majenta");
+        char1.walk(); 
         char1.Playing = "Uma Musume Pretty Derby"; 
         System.out.println(char1.Playing); 
         System.out.printf("I am %s, I love to eat %s and I like %s.", char1.getName(), char1.getFood(), char1.getColor()); 
